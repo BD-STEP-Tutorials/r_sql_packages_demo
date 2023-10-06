e47ccc594668381aad0988fbe075deab3a77f508
TRMichelson <123107239+TRMichelson@users.noreply.github.com>
2023-10-06 18:22:14 +0000

    Modified the db_create.R file to create the database.

--- db_create.R
+++ db_create.R
@@ -1,8 +1,15 @@
-setwd('workspaces/R_SQL_Demo')
+setwd('workspaces/R_SQL_Demo/data')
 
 library(DBI)
 
-mydb <- dbConnect(RSQLite::SQLite(), "synmedical.db")
+patients <- read.csv('patients.csv', header=TRUE, sep = ",")
+encounters <- read.csv('encounters.csv', header=TRUE, sep = ",")
 
-dbGetQuery(mydb, 'SELECT * FROM people')
+setwd('workspaces/R_SQL_Demo/database')
 
+mydb <- dbConnect(RSQLite::SQLite(), "synthetic.db")
+
+dbWriteTable(mydb, "patients", patients)
+dbWriteTable(mydb, "encounters", encounters)
+
+dbDisconnect(mydb)
