9e61d84aee039d6694c13542a53466b0b067b5da
TRMichelson <123107239+TRMichelson@users.noreply.github.com>
2023-10-06 18:25:07 +0000

    Changed the mysql extension to mysql3 editor. Modified R file that creates databases

--- db_create.R
+++ db_create.R
@@ -1,11 +1,11 @@
-setwd('workspaces/R_SQL_Demo/data')
+setwd('/workspaces/R_SQL_Demo/data')
 
 library(DBI)
 
 patients <- read.csv('patients.csv', header=TRUE, sep = ",")
 encounters <- read.csv('encounters.csv', header=TRUE, sep = ",")
 
-setwd('workspaces/R_SQL_Demo/database')
+setwd('/workspaces/R_SQL_Demo/database')
 
 mydb <- dbConnect(RSQLite::SQLite(), "synthetic.db")
 
